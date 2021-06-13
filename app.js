14dcc8fe1cd4f91815ca7752efe61bbea262d514
hariomkushwaha <hari2018cs048abesit@gmail.com>
2021-06-13 21:41:31 +0530

    finall

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const app = express();
 
 
 const PORT = process.env.PORT || 5000
-if (process.env.NODE_ENV === "production") {
-    app.use(compression())
-  }
-
-
-
 
 app.set('view engine', 'ejs');
 
