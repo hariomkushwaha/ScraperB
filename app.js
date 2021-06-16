7a68374f411da65eb58ec4f11f00784cd1c33b90
hariomkushwaha <hari2018cs048abesit@gmail.com>
2021-06-16 23:20:42 +0530

    added cors

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const bodyParser = require("body-parser");
 const cheerio = require("cheerio");
 const request = require("request");
 const { response } = require("express");
+const cors = require('cors');
 
 const app = express();
 
 
 const PORT = process.env.PORT || 5000
+app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 app.set('view engine', 'ejs');
 
